d5592bccad1f39e5e211f0eaaf6b312e6b321196
鄭翊宏 <>
2024-07-09 22:14:23 +0800

    主題選擇功能寫了一半 剩下一半 不知道為什麼要用classes自婊

--- src/script/themeController.js
+++ src/script/themeController.js
@@ -1,12 +1,42 @@
-const themeController = (arg)=>{
+import { DEFAULT_THEME } from "./const";
 
+class THEME_CONTROLLER {
+  static STORAGE_KEY = "theme";
+  static DATA_ATTR = "data-theme";
+  btn;
+  theme;
+  constructor(btn) {
+    this.btn = btn;
+    this.theme = this.btn.getAttribute(THEME_CONTROLLER.DATA_ATTR);
+  }
+  setTheme(e) {
+    localStorage.setItem(THEME_CONTROLLER.STORAGE_KEY, this.theme);
+    document.documentElement.setAttribute(
+      THEME_CONTROLLER.DATA_ATTR,
+      this.theme
+    );
+  }
 }
+export const themeInit = (arg) => {
+  const INIT_THEME = localStorage.getItem(THEME_CONTROLLER.STORAGE_KEY);
 
-const setTheme = (theme)=>{
+  if (INIT_THEME === null) {
+    document.documentElement.setAttribute(
+      THEME_CONTROLLER.DATA_ATTR,
+      DEFAULT_THEME
+    );
+    INIT_THEME = DEFAULT_THEME;
+  } else
+    document.documentElement.setAttribute(
+      THEME_CONTROLLER.DATA_ATTR,
+      INIT_THEME
+    );
 
-}
-
-export const themeInit = (arg)=>{
-  console.log(arg)
-  console.log(document)
-}
+  const THEME_BTN_LIST = document.querySelectorAll("button[data-theme]");
+  [...THEME_BTN_LIST].forEach((el) => {
+    const themeController = new THEME_CONTROLLER(el);
+    themeController.btn.addEventListener("click", (e) => {
+      themeController.setTheme(e);
+    });
+  });
+};
