855529c39b9b4cd38518b400d6e8bd5d2b287072
鄭翊宏 <>
2024-07-10 21:26:17 +0800

    themeController + header

---
+++ src/content/field/index.md
@@ -0,0 +1,5 @@
+---
+title:"早安你好"
+context:'fewfewfewfwefwefewfewfe'
+pageTitle:'Home'
+---
--- src/script/themeController.js
+++ src/script/themeController.js
@@ -1,53 +1,76 @@
 import { DEFAULT_THEME } from "./const";
-// 為什麼要用class 單純有點好奇javascript的類別
-class THEME_CONTROLLER {
+
+class ThemeController {
   static STORAGE_KEY = "theme";
   static DATA_ATTR = "data-theme";
+  static ACTIVE_CLASS = "bg-primary";
+  static THEME_BTN_LIST = document.querySelectorAll("button[data-theme]");
 
-  // button element for theme change
-  btn;
+  #btnList = [];
 
-  // atrribute data-theme's value from button element
-  theme;
-
-  // constructor take button element for arguments
-  constructor(btn) {
-    this.btn = btn;
-    this.theme = this.btn.getAttribute(THEME_CONTROLLER.DATA_ATTR);
+  addBtnElement(ThemeControllerDetail) {
+    this.#btnList.push(ThemeControllerDetail);
   }
-
-  // method sets the theme to root element
-  setTheme(e) {
-    localStorage.setItem(THEME_CONTROLLER.STORAGE_KEY, this.theme);
-    document.documentElement.setAttribute(
-      THEME_CONTROLLER.DATA_ATTR,
-      this.theme
-    );
+  get getBtnList() {
+    return this.#btnList;
   }
 }
 
+class ThemeControllerDetail {
+  btn;
+  theme;
+  constructor(btn) {
+    this.btn = btn;
+    this.theme = this.btn.getAttribute(ThemeController.DATA_ATTR);
+  }
+  removeBtnActive() {
+    this.btn.classList.remove(ThemeController.ACTIVE_CLASS);
+  }
+  addBtnActive() {
+    this.btn.classList.add(ThemeController.ACTIVE_CLASS);
+  }
+  setTheme() {
+    localStorage.setItem(ThemeController.STORAGE_KEY, this.theme);
+    document.documentElement.setAttribute(
+      ThemeController.DATA_ATTR,
+      this.theme
+    );
+    this.addBtnActive();
+  }
+  get getTheme() {
+    return this.theme;
+  }
+}
+
+const themeChange = (e, themeController) => {
+  themeController.getBtnList.forEach((btn) => {
+    btn.removeBtnActive();
+  });
+  e.setTheme();
+};
+
 // 頁面主題初始化
 export const themeInit = () => {
-  const INIT_THEME = localStorage.getItem(THEME_CONTROLLER.STORAGE_KEY);
-
+  const initTheme = localStorage.getItem(ThemeController.STORAGE_KEY);
+  const themeController = new ThemeController();
   // 如果先前沒有設定主題 則取預設主題
-  if (INIT_THEME === null) {
+  if (initTheme === null) {
     document.documentElement.setAttribute(
-      THEME_CONTROLLER.DATA_ATTR,
+      ThemeController.DATA_ATTR,
       DEFAULT_THEME
     );
+    initTheme = DEFAULT_THEME;
   } else
-    document.documentElement.setAttribute(
-      THEME_CONTROLLER.DATA_ATTR,
-      INIT_THEME
-    );
+    document.documentElement.setAttribute(ThemeController.DATA_ATTR, initTheme);
 
-  // 建立每個class
-  const THEME_BTN_LIST = document.querySelectorAll("button[data-theme]");
-  [...THEME_BTN_LIST].forEach((el) => {
-    const themeController = new THEME_CONTROLLER(el);
-    themeController.btn.addEventListener("click", (e) => {
-      themeController.setTheme(e);
-    });
+  ThemeController.THEME_BTN_LIST.forEach((el) => {
+    const themeControllerDetail = new ThemeControllerDetail(el);
+    themeController.addBtnElement(themeControllerDetail);
+    if (initTheme == themeControllerDetail.getTheme) {
+      themeControllerDetail.addBtnActive();
+    }
+    el.addEventListener("click", (e) =>
+      themeChange(themeControllerDetail, themeController)
+    );
   });
 };
