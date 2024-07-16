a96aac42af832cc0d2c97a179446e6c0684bd64e
鄭翊宏 <>
2024-07-16 21:43:24 +0800

    update...

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,24 @@
 // Import utilities from `astro:content`
 import { defineCollection, z } from "astro:content";
 // Define a `type` and `schema` for each collection
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+
+const filedCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    intro: z.string(),
+  }),
+});
+
 const postsCollection = defineCollection({
   type: "content",
   schema: ({ image }) =>
@@ -21,4 +39,5 @@ const postsCollection = defineCollection({
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   posts: postsCollection,
+  blogs: blogCollection,
 };
--- src/content/field/index.md
+++ src/content/field/index.md
@@ -1,5 +1,11 @@
 ---
-title:"早安你好"
-context:'fewfewfewfwefwefewfewfe'
-pageTitle:'Home'
+title: "My First Blog Post"
+intro: "hello my friend welcome to this site"
+pubDate: 2022-07-01
+description: "This is the first post of my new Astro blog."
+author: "Astro Learner"
+image:
+  url: "https://docs.astro.build/assets/rose.webp"
+  alt: "The Astro logo on a dark background with a pink glow."
+tags: ["astro", "blogging", "learning in public"]
 ---
